64565df069df29c08651f57a0249413f6a3c93dc
Adegbenga Adeye <adegbenga.adeye@outlook.com>
2020-03-08 22:39:40 +0100

    optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
-use std::io;
-use std::cmp::Ordering;
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
 use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
+    guess_game();
+    variables();
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("{}", MAX_POINTS);
+
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    ownersip();
+
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word will get the value 5
+    println!("{}", word);
+
+    s.clear();
+}
+
+fn variables() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+}
+
+fn guess_game() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -12,7 +46,8 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
@@ -32,3 +67,40 @@ fn main() {
         }
     }
 }
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn ownersip() {
+    let s = String::from("hello"); // s comes into scope
+
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
+
+    let x = 5; // x comes into scope
+
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it’s okay to still
+                   // use x afterward
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
